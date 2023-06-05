636558240e247812ac18bef26a36d02f622c0d80
beneyalraj <beneyalraj03@gmail.com>
2023-06-05 17:42:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import useState from "react";
+import React from "react";
+import data from './data'
+import Tours from "./components/Tours";
+
+const App = () => {
+
+  const[tours, setTours] = useState(data)
+
+  function removeTour(id){
+    const newTours = tours.filter(tour => tour.id !==id);
+    setTours(newTours);
+  }
+
+  return (
+    <div>
+      
+      <Tours tours = {tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Tours.js
@@ -0,0 +1,20 @@
+import Card from "./Card"
+
+function Tours({tours, removeTour}) {
+    return (
+        <div>
+            <div>
+                <h2>Tour Planner</h2>
+            </div>
+            <div>
+                {
+                    tours.map( (tour) => {
+                        return <Card {...tour} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tours; 
